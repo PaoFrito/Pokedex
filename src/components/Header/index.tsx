44504b2bc4e81d6f5179469d88592e53f3d9b8a9
Henrique Amaral <henrique.silva@pti.org.br>
2023-05-24 20:37:32 -0300

    header responsivo

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,43 @@
+import logo from "../../assets/logo.svg";
+import { Container } from "../Container";
+import styles from "./index.module.css";
+import { AiOutlineMenu } from "react-icons/ai"
+import { MenuModal } from "../MenuModal";
+import useMenuContext from "../../hooks/useMenuContext";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+
+  const { switchBoll} = useMenuContext();
+
+  return (
+    <header className={styles.header_background}>
+      <Container>
+        <div className={styles.header}>
+          <img className={styles.logo} src={logo} alt="Logo principal do sistema"/>
+          <AiOutlineMenu className={styles.icon} onClick={()=> switchBoll()}/>
+          <nav className={styles.nav}>
+            <Link className={styles.menu} to="/">
+              Home
+              <div className={styles.underline}/>  
+            </Link>  
+            <Link className={styles.menu} to="/">
+              Pokedex
+              <div className={styles.underline}/>
+            </Link> 
+            <Link className={styles.menu} to="/">
+              Lengendaries
+              <div className={styles.underline}/>
+            </Link>  
+            <Link className={styles.menu} to="https://pokeapi.co/docs/v2">
+              Documentation
+              <div className={styles.underline}/>
+            </Link>
+          </nav>
+        </div>
+      </Container>  
+      <MenuModal/>
+    </header>
+    
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
-import './index.css'
+import './index.module.css'
+import NotFound from './components/NotFound'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

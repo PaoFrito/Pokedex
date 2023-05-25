57c7d82db1693e7e241c10559b9f8d6fe03299be
Henrique Amaral <henrique.silva@pti.org.br>
2023-05-25 01:56:50 -0300

    home pronta

--- src/components/HomeComponent/index.tsx
+++ src/components/HomeComponent/index.tsx
@@ -1,30 +1,21 @@
 import styles from './index.module.css';
-import { Link } from 'react-router-dom';
-import banner from '../../assets/banner.png';
+import banner from '../../assets/Banner.png';
 import { Container } from '../Container';
 import { Button } from '../Buttons';
 
 export const HomeComponent = () => {
     return (
         <div className={styles.background}>
-            <img src={banner} alt="pikachu" className={styles.img}/>
-            <Container>
-                <main className={styles.main}>
-                    <h1>
-                        <b>Find</b> all your<br/>
-                        favorite<br/>
-                        <b>Pokemon</b>
-                    </h1>
-                    <p>
-                        You can know the type of Pokemon,<br />
-                        its strengths, disadvantages and<br />
-                        abilities
-                    </p>
-                    <Link to="/pokedex">
-                        <Button text='See Pokemons' type="success"/>
-                    </Link>
-                </main>
-            </Container>
+            <div className={styles.p_relative}>    
+                <img src={banner} alt="pikachu" className={styles.img}/>
+                <Container>
+                    <main className={styles.main}>
+                        <h1><b>Find</b> all your favorite <b>Pokemon</b></h1>
+                        <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+                        <Button link="/pokedex" text='See Pokemons' type="success"/>
+                    </main>
+                </Container>
+            </div>
         </div>
     );
 };
